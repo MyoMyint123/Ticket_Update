c78d4e4b1d8beac1db4027113976efe4f31f3fcb
Myo Myint Kyi <myomyint117mm@gmail.com>
2018-05-29 21:49:21 -0700

    2018 May 29

--- ticket.sql
+++ ticket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2018 at 08:12 AM
+-- Generation Time: May 30, 2018 at 06:47 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.0.21
 
@@ -46,14 +46,14 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`id`, `name`, `routes`, `departure_date`, `arrival_date`, `total_seat`, `available_seat`, `invalid_seats`, `price`, `images`) VALUES
-(1, 'Shwe Sin Set Kyar', 'Yangon-Mandalay', '2018-05-19 21:30:00', '2018-05-20 05:20:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 12000, 'dist/images/express_01.jpg'),
-(2, 'Mandalar Minn Express', 'Mandalay-Yangon', '2018-05-21 20:30:00', '2018-05-22 04:45:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 13000, 'dist/images/express_02.jpg'),
-(3, 'Shwe Sin Set Kyar', 'Yangon-Pakokku', '2018-05-19 21:30:00', '2018-05-20 05:20:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 12500, 'dist/images/express_03.jpg'),
-(4, 'Mandalar Minn Express', 'Monywa-Yangon', '2018-05-21 20:30:00', '2018-05-22 04:45:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 18000, 'dist/images/express_04.jpg'),
-(5, 'Shwe Mandalar', 'Yangon-Monywa', '2018-05-19 21:30:00', '2018-05-20 05:20:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 15500, 'dist/images/express_05.jpg'),
-(6, 'Shwe Sin Set Kyar', 'Pakokku-Yangon', '2018-05-21 20:30:00', '2018-05-22 04:45:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 20000, 'dist/images/express_06.jpg'),
-(7, 'Shwe Mandalar', 'Mandalay-Yangon', '2018-05-20 21:30:00', '2018-05-21 05:20:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 17500, 'dist/images/express_02.jpg'),
-(8, 'Mandalar Minn Express', 'Yangon-Pakokku', '2018-05-21 22:30:00', '2018-05-22 02:45:00', 44, 30, '2,3,7,11,13,17,19,23,29,31,37,41,43', 12000, 'dist/images/express_04.jpg');
+(1, 'Shwe Sin Set Kyar', 'Yangon-Mandalay', '2018-05-30 21:30:00', '2018-05-31 05:20:00', 44, 31, '2,3,7,11,13,17,19,23,29,31,37,41,43', 12000, 'dist/images/express_01.jpg'),
+(2, 'Mandalar Minn Express', 'Mandalay-Yangon', '2018-05-31 23:30:00', '2018-06-01 04:45:00', 44, 27, '2,3,7,11,13,17,19,23,29,31,37,41,43,1,5,9,6', 13000, 'dist/images/express_02.jpg'),
+(3, 'Shwe Sin Set Kyar', 'Yangon-Pakokku', '2018-06-02 23:30:00', '2018-06-03 06:20:00', 44, 26, '2,3,7,11,13,17,19,23,29,31,37,41,43,1,5,9,6,10', 12500, 'dist/images/express_03.jpg'),
+(4, 'Mandalar Minn Express', 'Monywa-Yangon', '2018-06-01 20:30:00', '2018-06-02 04:45:00', 44, 31, '2,3,7,11,13,17,19,23,29,31,37,41,43', 18000, 'dist/images/express_04.jpg'),
+(5, 'Shwe Mandalar', 'Yangon-Monywa', '2018-05-29 10:30:00', '2018-05-29 19:20:00', 44, 31, '2,3,7,11,13,17,19,23,29,31,37,41,43', 15500, 'dist/images/express_05.jpg'),
+(6, 'Shwe Sin Set Kyar', 'Pakokku-Yangon', '2018-05-30 10:30:00', '2018-05-30 19:45:00', 44, 31, '2,3,7,11,13,17,19,23,29,31,37,41,43', 20000, 'dist/images/express_06.jpg'),
+(7, 'Shwe Mandalar', 'Mandalay-Yangon', '2018-06-04 21:30:00', '2018-06-05 05:20:00', 44, 31, '2,3,7,11,13,17,19,23,29,31,37,41,43', 17500, 'dist/images/express_02.jpg'),
+(8, 'Mandalar Minn Express', 'Yangon-Pakokku', '2018-05-31 09:00:00', '2018-05-31 05:45:00', 44, 31, '2,3,7,11,13,17,19,23,29,31,37,41,43', 12000, 'dist/images/express_04.jpg');
 
 -- --------------------------------------------------------
 
@@ -75,8 +75,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `car_id`, `name`, `email`, `phone`, `seat_numbers`) VALUES
-(22, 7, 'Myo Myint Kyi', 'lionel.myogyi@gmail.com', '0987837838', '25,26,27,28,30,32'),
-(23, 7, 'fdsa', 'df@f', '34343', '21,22,24,18');
+(27, 3, 'admin', 'admin@gmail.com', '09773883673', '1,5,9,6,10'),
+(28, 2, 'man to ygn', 'mantoyan@gmail.com', '09876564534', '1,5,9,6');
 
 --
 -- Indexes for dumped tables
@@ -107,7 +107,7 @@ ALTER TABLE `cars`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
